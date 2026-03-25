a4c2307c735a18546eff3c35c72d948a784a4b0d
Vishwak Velamuri <45471435+vishwakvel@users.noreply.github.com>
2026-03-25 19:43:01 +0000

    [LeetCode Sync] Runtime - 25 ms (44.44%), Memory - 25.6 MB (54.08%)

---
+++ leetcode/0238-product-of-array-except-self/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        answer = [1] * len(nums)
+        prefix = 1
+        suffix = 1
+
+        for i in range(len(nums)):
+            answer[i] *= prefix
+            prefix *= nums[i]
+        
+        for i in range(len(nums) - 1, -1, -1):
+            answer[i] *= suffix
+            suffix *= nums[i]
+        
+        return answer
