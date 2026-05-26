e8e84410bc222684691b4180bde75b6bb3adaffd
Vishwak Velamuri <45471435+vishwakvel@users.noreply.github.com>
2026-05-26 21:50:46 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 13 MB (82.28%)

--- leetcode/0001-two-sum/solution.py
+++ leetcode/0001-two-sum/solution.py
@@ -5,15 +5,20 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        if len(nums) == 2:
-            return [0,1]
-        
-        ndict = {}
+        """
+        Strategy:
+        1. Empty dictionary to store numbers as they are processed in for loop. Use dict because you want to also store the index and also because .get() is a O(1) process which makes it faster.
+        2. Use a for loop to go through the nums list and for each num calculate the difference needed to satisfy the target.
+        3. Check if the difference already exists in the ans dict
+            a) If it does, then return the current num's index along with the index of the difference (which is stored in the ans dict as the value)
+            b) If not, then add the num:index pair to the ans dict
+        """
+        ans = {}
 
-        for i, num in enumerate(nums):
+        for index, num in enumerate(nums):
             diff = target - num
 
-            if diff in ndict:
-                return [ndict[diff], i]
-
-            ndict[num] = i
+            if diff in ans:
+                return [ans.get(diff), index]
+            else:
+                ans[num] = index
