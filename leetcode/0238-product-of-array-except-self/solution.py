15dca4da68052f529e65e89fcc3fa9dd9c30a120
Vishwak Velamuri <45471435+vishwakvel@users.noreply.github.com>
2026-05-26 23:20:36 +0000

    [LeetCode Sync] Runtime - 17 ms (80.24%), Memory - 25.3 MB (85.56%)

--- leetcode/0238-product-of-array-except-self/solution.py
+++ leetcode/0238-product-of-array-except-self/solution.py
@@ -1,15 +1,25 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        answer = [1] * len(nums)
-        prefix = 1
-        suffix = 1
+        """
+        1. Create ans list that is same length as nums list and default val is set to 1
+        2. Go through left first, so multiply ans[i] with each element in nums that is to the left
+            a) Have accumulator that you change everytime
+            b) Update ans[i] first then change acc
+        3. Go through right next, so multiply ans[i] with each element in nums that is to the right
+            a) Have accumulator that you change everytime (multiply because you have to keep left's progress)
+            b) update ans[i] first then change acc
+        """
+        nums_len = len(nums)
+        ans = [1] * nums_len
 
-        for i in range(len(nums)):
-            answer[i] *= prefix
-            prefix *= nums[i]
+        left = 1
+        for i in range(nums_len):
+            ans[i] = left
+            left *= nums[i]
         
-        for i in range(len(nums) - 1, -1, -1):
-            answer[i] *= suffix
-            suffix *= nums[i]
+        right = 1
+        for i in range(nums_len - 1, -1, -1):
+            ans[i] *= right
+            right *= nums[i]
         
-        return answer
+        return ans
