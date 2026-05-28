90cacdc1b9e0e1d7429b222bf67d1a353f0dc63e
Vishwak Velamuri <45471435+vishwakvel@users.noreply.github.com>
2026-05-28 21:30:54 +0000

    [LeetCode Sync] Runtime - 11 ms (82.56%), Memory - 22.1 MB (52.76%)

--- leetcode/0049-group-anagrams/solution.py
+++ leetcode/0049-group-anagrams/solution.py
@@ -1,13 +1,16 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        strsdict = {}
+        """
+        1. Create a defaultdict with empty list set as the base value
+        2. Go through each element in strs and sort the str alphabetically
+            a) Keys are the sorted string and values are the anagrams in the strs list
+        3. Add str to dict using sorted str as the key and appending the str to the respective value
+        4. Return list of dict.values() (convert to list because .values() is a diff view type)
+        """
+        ans = defaultdict(list)
 
         for s in strs:
-            sorted_s = tuple(sorted(s)) # cant use list so use tuple
-
-            if sorted_s not in strsdict: # default dict to set key value
-                strsdict[sorted_s] = []
-
-            strsdict[sorted_s].append(s) # key = sorted string value = s (from str)
-
-        return list(strsdict.values())
+            sorted_s = ''.join(sorted(s))
+            ans[sorted_s].append(s)
+        
+        return list(ans.values())
