04d42ca41cd7d55b3092dc29a1ba44200043f374
Vishwak Velamuri <45471435+vishwakvel@users.noreply.github.com>
2026-05-28 21:29:53 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 13.2 MB (53.22%)

--- leetcode/0001-two-sum/solution.py
+++ leetcode/0001-two-sum/solution.py
@@ -1,11 +1,6 @@
 class Solution(object):
     def twoSum(self, nums, target):
         """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        """
         Strategy:
         1. Empty dictionary to store numbers as they are processed in for loop. Use dict because you want to also store the index and also because .get() is a O(1) process which makes it faster.
         2. Use a for loop to go through the nums list and for each num calculate the difference needed to satisfy the target.
@@ -15,10 +10,10 @@ class Solution(object):
         """
         ans = {}
 
-        for index, num in enumerate(nums):
-            diff = target - num
+        for i in range(len(nums)):
+            diff = target - nums[i]
 
             if diff in ans:
-                return [ans.get(diff), index]
+                return [ans.get(diff), i]
             else:
-                ans[num] = index
+                ans[nums[i]] = i
