7aba7a828fa1fe6d3daf323c5bbd450a5e6774f8
Vishwak Velamuri <45471435+vishwakvel@users.noreply.github.com>
2026-06-06 23:01:49 +0000

    [LeetCode Sync] Runtime - 3 ms (94.51%), Memory - 21.3 MB (11.21%)

--- leetcode/0042-trapping-rain-water/solution.py
+++ leetcode/0042-trapping-rain-water/solution.py
@@ -9,22 +9,18 @@ class Solution:
         """
         left = 0
         right = len(height) - 1
-        left_max = 0
-        right_max = 0
+        left_max = height[left]
+        right_max = height[right]
         water = 0
 
         while left < right:
-            if height[left] < height[right]:
-                if height[left] >= left_max:
-                    left_max = height[left]
-                else:
-                    water += left_max - height[left]
+            if left_max < right_max:
                 left += 1
+                left_max = max(left_max, height[left])
+                water += left_max - height[left]
             else:
-                if height[right] >= right_max:
-                    right_max = height[right]
-                else:
-                    water += right_max - height[right]
                 right -= 1
+                right_max = max(right_max, height[right])
+                water += right_max - height[right]
 
         return water
