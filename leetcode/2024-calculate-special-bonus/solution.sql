5e5a8033ebc5b0a3abeb3042e3162d7801157d20
Vishwak Velamuri <45471435+vishwakvel@users.noreply.github.com>
2026-06-13 18:16:39 +0000

    [LeetCode Sync] Runtime - 362 ms (20.70%), Memory - 0.0B (100.00%)

---
+++ leetcode/2024-calculate-special-bonus/solution.sql
@@ -0,0 +1,8 @@
+-- Write your PostgreSQL query statement below
+SELECT employee_id,
+    CASE
+        WHEN employee_id % 2 <> 0 AND LEFT(name, 1) NOT LIKE 'M' THEN salary
+        ELSE 0
+    END AS bonus
+FROM Employees
+ORDER BY employee_id
