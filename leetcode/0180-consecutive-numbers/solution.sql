fbce94d85592af3157ade69f463a5ca59dc0011b
Vishwak Velamuri <45471435+vishwakvel@users.noreply.github.com>
2026-06-14 21:02:43 +0000

    [LeetCode Sync] Runtime - 262 ms (68.40%), Memory - 0.0B (100.00%)

---
+++ leetcode/0180-consecutive-numbers/solution.sql
@@ -0,0 +1,10 @@
+-- Write your PostgreSQL query statement below
+SELECT DISTINCT num AS ConsecutiveNums
+FROM (
+    SELECT
+        num,
+        LEAD(num, 1) OVER (ORDER BY id) AS next1,
+        LEAD(num, 2) OVER (ORDER BY id) AS next2
+    FROM Logs
+) t
+WHERE num = next1 and num = next2
