8d3b301620c8b51b959e6ba038e580f11e2a4ec2
Vishwak Velamuri <45471435+vishwakvel@users.noreply.github.com>
2026-06-25 01:16:35 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.6 MB (5.37%)

---
+++ leetcode/0090-subsets-ii/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        ans = []
+
+        def backtrack(start: int, path: List[int]):
+            ans.append(path[:])
+
+            for i in range(start, len(nums)):
+                if i > start and nums[i] == nums[i - 1]:
+                    continue
+                    
+                path.append(nums[i])
+                backtrack(i + 1, path)
+                path.pop()
+        
+        backtrack(0, [])
+        return ans
