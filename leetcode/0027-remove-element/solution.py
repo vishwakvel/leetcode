26f3cc239978a15cea95d914a3099e5f60ca9a54
Vishwak Velamuri <45471435+vishwakvel@users.noreply.github.com>
2026-06-27 21:56:45 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.3 MB (19.35%)

---
+++ leetcode/0027-remove-element/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        write = 0
+
+        for read in range(len(nums)):
+            if nums[read] != val:
+                nums[write] = nums[read]
+                write += 1
+        
+        return write
