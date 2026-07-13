aded716d2eee3cbba47c4e975f81c5bdc42cec88
Vishwak Velamuri <45471435+vishwakvel@users.noreply.github.com>
2026-07-13 23:01:54 +0000

    [LeetCode Sync] Runtime - 63 ms (26.04%), Memory - 28.8 MB (20.60%)

--- leetcode/0121-best-time-to-buy-and-sell-stock/solution.py
+++ leetcode/0121-best-time-to-buy-and-sell-stock/solution.py
@@ -1,22 +1,12 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        """
-        1. Sliding window, so we set buy as first element and sell as second.
-        2. We check and see if sell is greater than buy
-            a) If it is, then we calculate the profit and make sure to store the max of the current and previous in profit.
-            b) If it isn't, then we move buy up to sell because why would we have that as our buy price when theres a smaller number after it
-        3. We increment sell by 1
-        """
         buy = 0
-        sell = 1
         profit = 0
 
-        while sell < len(prices):
-            if prices[sell] > prices[buy]:
-                profit = max(profit, prices[sell] - prices[buy])
-            else:
-                buy = sell
+        for sell in range(1, len(prices)):
+            profit = max(profit, prices[sell] - prices[buy])
 
-            sell += 1
+            if prices[sell] < prices[buy]:
+                buy = sell
 
         return profit
