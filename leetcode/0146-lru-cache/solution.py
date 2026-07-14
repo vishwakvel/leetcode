7894cf78fb431f147fc033df9b1d7d40d40ccbf9
Vishwak Velamuri <45471435+vishwakvel@users.noreply.github.com>
2026-07-14 02:08:10 +0000

    [LeetCode Sync] Runtime - 134 ms (37.77%), Memory - 77.3 MB (37.17%)

---
+++ leetcode/0146-lru-cache/solution.py
@@ -0,0 +1,60 @@
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.hashmap = {}
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def remove(self, node):
+        prev = node.prev
+        nxt = node.next
+
+        prev.next = nxt
+        nxt.prev = prev
+    
+    def insert(self, node):
+        first = self.head.next
+
+        node.prev = self.head
+        node.next = first
+
+        self.head.next = node
+        first.prev = node
+
+    def get(self, key: int) -> int:
+        if key not in self.hashmap:
+            return -1
+        
+        node = self.hashmap[key]
+        self.remove(node)
+        self.insert(node)
+
+        return node.value
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.hashmap:
+            self.remove(self.hashmap[key])
+
+        node = Node(key, value)
+        self.hashmap[key] = node
+        self.insert(node)
+
+        if len(self.hashmap) > self.capacity:
+            lru = self.tail.prev
+            self.remove(lru)
+            del self.hashmap[lru.key]
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
