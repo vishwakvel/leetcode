9b9d434e45a5c8b4630a16e697d89a42491c78c2
Vishwak Velamuri <45471435+vishwakvel@users.noreply.github.com>
2026-07-14 14:30:48 +0000

    [LeetCode Sync] Runtime - 29 ms (79.93%), Memory - 34.3 MB (34.63%)

--- leetcode/0208-implement-trie-prefix-tree/solution.py
+++ leetcode/0208-implement-trie-prefix-tree/solution.py
@@ -1,41 +1,49 @@
 class TrieNode:
     def __init__(self):
         self.children = {}
-        self.is_end = False
+        self.isEnd = False
 
 class Trie:
+
     def __init__(self):
         self.root = TrieNode()
 
     def insert(self, word: str) -> None:
-        node = self.root
+        curr = self.root
 
         for char in word:
-            if char not in node.children:
-                node.children[char] = TrieNode()
-
-            node = node.children[char]
-
-        node.is_end = True
+            if char not in curr.children:
+                curr.children[char] = TrieNode()
+            
+            curr = curr.children[char]
+        
+        curr.isEnd = True
 
     def search(self, word: str) -> bool:
-        node = self.root
+        curr = self.root
 
         for char in word:
-            if char not in node.children:
+            if char not in curr.children:
                 return False
-
-            node = node.children[char]
-
-        return node.is_end
+            
+            curr = curr.children[char]
+    
+        return curr.isEnd
 
     def startsWith(self, prefix: str) -> bool:
-        node = self.root
+        curr = self.root
 
         for char in prefix:
-            if char not in node.children:
+            if char not in curr.children:
                 return False
-
-            node = node.children[char]
-
+            
+            curr = curr.children[char]
+        
         return True
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
