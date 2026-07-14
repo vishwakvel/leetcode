434afc25ec0972db1b4e3884a6e4fa95dfa569db
Vishwak Velamuri <45471435+vishwakvel@users.noreply.github.com>
2026-07-14 00:41:29 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.6 MB (16.13%)

---
+++ leetcode/0232-implement-queue-using-stacks/solution.py
@@ -0,0 +1,40 @@
+from collections import deque
+
+class MyQueue:
+
+    def __init__(self):
+        self.input = []
+        self.output = []
+
+
+    def push(self, x: int) -> None:
+        self.input.append(x)
+
+    def pop(self) -> int:
+        if not self.output:
+            while self.input:
+                self.output.append(self.input.pop())
+
+        return self.output.pop()
+
+    def peek(self) -> int:
+        if not self.output:
+            while self.input:
+                self.output.append(self.input.pop())
+            
+        temp = self.output.pop()
+        self.output.append(temp)
+
+        return temp
+
+    def empty(self) -> bool:
+        return not self.input and not self.output
+        
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
