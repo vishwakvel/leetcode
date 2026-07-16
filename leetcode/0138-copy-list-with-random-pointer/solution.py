3a3ccf7feb7981a7b9b22b39e20b0f9d97e0e8c6
Vishwak Velamuri <45471435+vishwakvel@users.noreply.github.com>
2026-07-16 02:30:04 +0000

    [LeetCode Sync] Runtime - 58 ms (6.54%), Memory - 20 MB (88.27%)

--- leetcode/0138-copy-list-with-random-pointer/solution.py
+++ leetcode/0138-copy-list-with-random-pointer/solution.py
@@ -11,20 +11,20 @@ class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
         if not head:
             return None
+
+        hmap = {} # old to new node
+        curr = head
         
-        hashmap = {}
+        while curr:
+            node = Node(curr.val, curr.next, curr.random)
+            hmap[curr] = node
+            curr = curr.next
+        
         curr = head
 
         while curr:
-            hashmap[curr] = Node(curr.val)
-            curr = curr.next
-
-        curr = head
-
-        while curr:
-            copy = hashmap[curr]
-            copy.next = hashmap.get(curr.next)
-            copy.random = hashmap.get(curr.random)
+            hmap[curr].next = hmap.get(curr.next)
+            hmap[curr].random = hmap.get(curr.random)
             curr = curr.next
         
-        return hashmap[head]
+        return hmap[head]
