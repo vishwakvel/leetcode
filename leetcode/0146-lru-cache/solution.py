459ddbfe0cd84a998d7ebc1e49e50f75206eac9f
Vishwak Velamuri <45471435+vishwakvel@users.noreply.github.com>
2026-07-16 23:38:23 +0000

    [LeetCode Sync] Runtime - 107 ms (72.80%), Memory - 77.2 MB (44.09%)

--- leetcode/0146-lru-cache/solution.py
+++ leetcode/0146-lru-cache/solution.py
@@ -9,11 +9,20 @@ class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
+        self.hashmap = {}
         self.head = Node(0, 0)
         self.tail = Node(0, 0)
         self.head.next = self.tail
         self.tail.prev = self.head
-        self.hashmap = {} # key to node
+
+    def insert(self, node):
+        first = self.head.next
+
+        node.prev = self.head
+        node.next = first
+
+        self.head.next = node
+        first.prev = node
     
     def remove(self, node):
         prev = node.prev
@@ -22,31 +31,23 @@ class LRUCache:
         prev.next = nxt
         nxt.prev = prev
 
-    def insert(self, node):
-        first = self.head.next
-
-        node.next = first
-        node.prev = self.head
-
-        self.head.next = node
-        first.prev = node
-
     def get(self, key: int) -> int:
         if key not in self.hashmap:
             return -1
         
-        self.remove(self.hashmap[key])
-        self.insert(self.hashmap[key])
-
-        return self.hashmap[key].value
+        node = self.hashmap[key]
+        self.remove(node)
+        self.insert(node)
+        return node.value
 
     def put(self, key: int, value: int) -> None:
         if key in self.hashmap:
             self.remove(self.hashmap[key])
+            del self.hashmap[key]
         
         node = Node(key, value)
-        self.hashmap[key] = node
         self.insert(node)
+        self.hashmap[key] = node
 
         if len(self.hashmap) > self.capacity:
             lru = self.tail.prev
