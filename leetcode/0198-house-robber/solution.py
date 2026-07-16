80f1a8aa90c3dc982f3ec4c508cabe300437cc12
Vishwak Velamuri <45471435+vishwakvel@users.noreply.github.com>
2026-07-16 17:48:38 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.2 MB (62.05%)

--- leetcode/0198-house-robber/solution.py
+++ leetcode/0198-house-robber/solution.py
@@ -1,5 +1,6 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        """
         money = {}
 
         def dp(house):
@@ -14,3 +15,15 @@ class Solution:
             return money[house]
         
         return dp(0)
+"""
+        if len(nums) == 1:
+            return nums[-1]
+            
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+
+        for i in range(2, len(nums)):
+            dp[i] = max(nums[i] + dp[i-2], dp[i-1])
+        
+        return dp[-1]
