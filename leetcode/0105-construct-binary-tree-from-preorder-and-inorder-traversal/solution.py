42270118cc43808a83570e2c7bd6a41d4f8a0c36
Vishwak Velamuri <45471435+vishwakvel@users.noreply.github.com>
2026-07-16 02:55:37 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 21.1 MB (55.37%)

--- leetcode/0105-construct-binary-tree-from-preorder-and-inorder-traversal/solution.py
+++ leetcode/0105-construct-binary-tree-from-preorder-and-inorder-traversal/solution.py
@@ -1,24 +1,26 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def buildTree(self, preorder, inorder):
-        """
-        :type preorder: List[int]
-        :type inorder: List[int]
-        :rtype: Optional[TreeNode]
-        """
-        if not preorder:
-            return None
-        
-        root_val = preorder[0]
-        root = TreeNode(root_val)
-        mid = inorder.index(root_val)
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        hmap = {val:i for i, val in enumerate(inorder)}
+        p = 0
 
-        root.left = self.buildTree(preorder[1:mid+1], inorder[:mid])
-        root.right = self.buildTree(preorder[mid+1:], inorder[mid+1:])
+        def dfs(l, r):
+            nonlocal p
 
-        return root
+            if l > r:
+                return None
+            
+            root_val = preorder[p]
+            p += 1
+            root = TreeNode(root_val)
+            mid = hmap[root_val]
+            root.left = dfs(l, mid-1)
+            root.right = dfs(mid+1, r)
+            return root
+
+        return dfs(0, len(inorder)-1)
