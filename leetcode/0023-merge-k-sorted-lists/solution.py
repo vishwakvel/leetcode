7f50222bc633fbfad537f6bd9cddcf9b34383ace
Vishwak Velamuri <45471435+vishwakvel@users.noreply.github.com>
2026-07-17 21:24:29 +0000

    [LeetCode Sync] Runtime - 3 ms (99.11%), Memory - 23 MB (46.81%)

--- leetcode/0023-merge-k-sorted-lists/solution.py
+++ leetcode/0023-merge-k-sorted-lists/solution.py
@@ -3,41 +3,30 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+import heapq
+
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        def mergeTwoLists(list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-            dummy = ListNode()
-            tail = dummy
-
-            while list1 and list2:
-                if list1.val <= list2.val:
-                    tail.next = list1
-                    list1 = list1.next
-                else:
-                    tail.next = list2
-                    list2 = list2.next
-                
-                tail = tail.next
-            
-            if list1:
-                tail.next = list1
-            else:
-                tail.next = list2
-            
-            return dummy.next
-        
         if not lists:
             return None
+
+        heap = []
+
+        for i, node in enumerate(lists):
+            if node:
+                heapq.heappush(heap, (node.val, i, node))
         
-        while len(lists) > 1:
-            mergedLists = []
+        dummy = ListNode()
+        curr = dummy
 
-            for i in range(0, len(lists), 2):
-                l1 = lists[i]
-                l2 = lists[i + 1] if i + 1 < len(lists) else None
+        while heap:
+            val, i, node = heapq.heappop(heap)
 
-                mergedLists.append(mergeTwoLists(l1, l2))
-                
-            lists = mergedLists
+            curr.next = node
+            curr = curr.next
+
+            if node.next:
+                heapq.heappush(heap, (node.next.val, i, node.next))
             
-        return lists[0]
+        return dummy.next
