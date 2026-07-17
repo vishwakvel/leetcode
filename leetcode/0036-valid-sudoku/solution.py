45fa561b1694613d4bc3475f293637f9127788f9
Vishwak Velamuri <45471435+vishwakvel@users.noreply.github.com>
2026-07-17 21:45:41 +0000

    [LeetCode Sync] Runtime - 3 ms (74.07%), Memory - 19.2 MB (94.87%)

--- leetcode/0036-valid-sudoku/solution.py
+++ leetcode/0036-valid-sudoku/solution.py
@@ -1,33 +1,21 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        """
-        1. For each row, column, and 3x3 box, have a defaultdict with the default value being an empty set and the keys are i and j (row/col numbers)
-        2. Iterate through every single square
-            a) If its a "." continue
-            b) Check if the number in that square is already in any of the dicts
-                a) If it is, return False
-                b) If not, add the key value pair (index: value) to each of the dicts
-        3. If it goes through iteration fully, then it passed the tests so return True
-        """
-
         rows = defaultdict(set)
         cols = defaultdict(set)
-        boxes = defaultdict(set)
+        squares = defaultdict(set)
 
         for i in range(9):
             for j in range(9):
-                val = board[i][j]
-
-                if val == ".":
+                num = board[i][j]
+                
+                if num == ".":
                     continue
-
-                box = (i // 3, j // 3)
-
-                if (val in rows[i] or val in cols[j] or val in boxes[box]):
+                
+                if num in rows[i] or num in cols[j] or num in squares[(i//3, j//3)]:
                     return False
-
-                rows[i].add(val)
-                cols[j].add(val)
-                boxes[box].add(val)
+                
+                rows[i].add(num)
+                cols[j].add(num)
+                squares[(i//3,j//3)].add(num)
         
         return True
