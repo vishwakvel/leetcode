3807ec2af2c52a838ca979420de6b81e1b9c2dfc
Vishwak Velamuri <45471435+vishwakvel@users.noreply.github.com>
2026-07-17 01:19:47 +0000

    [LeetCode Sync] Runtime - 7 ms (75.93%), Memory - 19.5 MB (83.04%)

--- leetcode/0039-combination-sum/solution.py
+++ leetcode/0039-combination-sum/solution.py
@@ -2,19 +2,18 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         ans = []
 
-        def backtrack(start, remaining, path):
-            if remaining == 0:
+        def backtracking(index, target, path):
+            if target == 0:
                 ans.append(path.copy())
                 return
             
-            if remaining < 0:
+            if target < 0:
                 return
             
-            for i in range(start, len(candidates)):
+            for i in range(index, len(candidates)):
                 path.append(candidates[i])
-                backtrack(i, remaining-candidates[i], path)
+                backtracking(i, target-candidates[i], path)
                 path.pop()
-        
-        backtrack(0, target, [])
 
+        backtracking(0, target, [])
         return ans
