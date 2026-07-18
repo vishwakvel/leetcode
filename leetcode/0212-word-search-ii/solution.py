1ff6e2ec53f9df53dd8d25108095444047fb79fe
Vishwak Velamuri <45471435+vishwakvel@users.noreply.github.com>
2026-07-18 01:10:35 +0000

    [LeetCode Sync] Runtime - 4733 ms (56.21%), Memory - 21.8 MB (15.15%)

--- leetcode/0212-word-search-ii/solution.py
+++ leetcode/0212-word-search-ii/solution.py
@@ -1,48 +1,53 @@
 class TrieNode:
     def __init__(self):
         self.children = {}
-        self.word = None
+        self.isEnd = False
 
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
         root = TrieNode()
+        m = len(board)
+        n = len(board[0])
 
-        # Build trie
         for word in words:
-            node = root
-            for ch in word:
-                if ch not in node.children:
-                    node.children[ch] = TrieNode()
-                node = node.children[ch]
-            node.word = word
+            curr = root
 
-        rows, cols = len(board), len(board[0])
-        res = []
-
-        def dfs(r, c, node):
-            ch = board[r][c]
-
-            if ch not in node.children:
+            for char in word:
+                if char not in curr.children:
+                    curr.children[char] = TrieNode()
+                
+                curr = curr.children[char]
+            
+            curr.isEnd = True
+        
+        ans = set()
+        
+        def backtracking(r, c, node, path):
+            if r < 0 or c < 0 or r >= m or c >= n:
                 return
+            
+            char = board[r][c]
 
-            node = node.children[ch]
-
-            if node.word:
-                res.append(node.word)
-                node.word = None  # prevent duplicates
-
+            if char == "#" or char not in node.children:
+                return
+            
+            node = node.children[char]
+            path += char
+            
+            if node.isEnd:
+                ans.add(path)
+            
             board[r][c] = "#"
 
-            for dr, dc in ((1, 0), (-1, 0), (0, 1), (0, -1)):
-                nr, nc = r + dr, c + dc
+            backtracking(r + 1, c, node, path)
+            backtracking(r - 1, c, node, path)
+            backtracking(r, c + 1, node, path)
+            backtracking(r, c - 1, node, path)
 
-                if (0 <= nr < rows and 0 <= nc < cols and board[nr][nc] != "#"):
-                    dfs(nr, nc, node)
-
-            board[r][c] = ch
-
-        for r in range(rows):
-            for c in range(cols):
-                dfs(r, c, root)
-
-        return res
+            board[r][c] = char
+        
+        for r in range(m):
+            for c in range(n):
+                backtracking(r, c, root, "")
+        
+        return list(ans)
