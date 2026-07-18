c08d77278dc6cc6e5bffa09da34c6172432a5e49
Vishwak Velamuri <45471435+vishwakvel@users.noreply.github.com>
2026-07-18 19:12:20 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.2 MB (52.81%)

--- leetcode/0070-climbing-stairs/solution.py
+++ leetcode/0070-climbing-stairs/solution.py
@@ -1,12 +1,14 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+        # state represents number of diff ways to climb i stairs
         if n <= 2:
             return n
             
-        dp = [1] * n
-        dp[1] = 2
+        dp = [0] * (n+1)
+        dp[1] = 1
+        dp[2] = 2
 
-        for i in range(2, n):
-            dp[i] = dp[i-1] + dp[i-2]
-
-        return dp[n-1]
+        for i in range(3, n+1):
+            dp[i] += dp[i-1] + dp[i-2]
+        
+        return dp[-1]
