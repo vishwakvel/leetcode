4d12ca340ed67e1e206ef7044b196b2857ab14ec
Vishwak Velamuri <45471435+vishwakvel@users.noreply.github.com>
2026-07-18 17:14:32 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.4 MB (40.69%)

--- leetcode/0017-letter-combinations-of-a-phone-number/solution.py
+++ leetcode/0017-letter-combinations-of-a-phone-number/solution.py
@@ -1,25 +1,30 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        phone = {
-            "2": "abc",
-            "3": "def",
-            "4": "ghi",
-            "5": "jkl",
-            "6": "mno",
-            "7": "pqrs",
-            "8": "tuv",
-            "9": "wxyz"
+        letters = {
+            "2": ["a", "b", "c"],
+            "3": ["d", "e", "f"],
+            "4": ["g", "h", "i"],
+            "5": ["j", "k", "l"],
+            "6": ["m", "n", "o"],
+            "7": ["p", "q", "r", "s"],
+            "8": ["t", "u", "v"],
+            "9": ["w", "x", "y", "z"]
         }
-        
+
         ans = []
 
-        def backtrack(index, path):
+        def backtrack(path, index):
             if index == len(digits):
-                ans.append(path[:])
+                ans.append(path)
                 return
             
-            for letter in phone[digits[index]]:
-                backtrack(index + 1, path + letter)
+            if digits[index] not in letters:
+                return
+            
+            for letter in letters[digits[index]]:
+                path += letter
+                backtrack(path, index+1)
+                path = path[:len(path)-1]
         
-        backtrack(0, "")
+        backtrack("", 0)
         return ans
