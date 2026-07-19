75f86436fda7acd9f4cef7839ba95bae136a4149
Vishwak Velamuri <45471435+vishwakvel@users.noreply.github.com>
2026-07-19 03:18:22 +0000

    [LeetCode Sync] Runtime - 71 ms (51.22%), Memory - 19.7 MB (73.96%)

--- leetcode/0076-minimum-window-substring/solution.py
+++ leetcode/0076-minimum-window-substring/solution.py
@@ -1,44 +1,38 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        """
-        1. Create counter for t to keep track of chars as well as how many of each appear. Also create an empty counter to fill with the window's chars. Formed var is to keep track of how many chars have been fully taken care of.
-        2. For each right char, add to window Counter
-        3. Check if that char is in need Counter and if need and window values for that char are equal (aka if count for both chars are the same)
-            a) If true, then increase formed by 1 since that means 1 more char has been fully taken care of
-            b) If not, continue
-        4. Then, we need to greedily shorten a valid substring we find and see if we can find a smaller one. For this, we use a while loop and check if the length of this substring is smaller than the global min.
-            a) If it is, then replace the min_len and ans with the current ones we just found
-            b) If not, continue
-        5. Then we prepare to move to the left, so first we remove the char from the window (just 1 of it not the whole char)
-        6. Check if left char is in need Counter and if the window's value of left char is less than need's value of left char
-            a) If it is, then decrease formed by 1
-            b) If it isn't, continue
-        7. Finally increase left by 1 so move window to the right
-        """
-        
-        need = Counter(t)
-        window = Counter()
+        m = len(s)
+        n = len(t)
+        tmap = defaultdict(int)
         formed = 0
+        
+        if n > m:
+            return ""
+
+        for char in t:
+            tmap[char] += 1
+        
         left = 0
-        min_len = float("inf")
-        ans = ""
+        smap = defaultdict(int)
+        ans = (float("inf"), None, None)
 
-        for right in range(len(s)):
-            window[s[right]] += 1
+        for right in range(m):
+            char = s[right]
+            smap[char] += 1
 
-            if s[right] in need and window[s[right]] == need[s[right]]:
+            if char in tmap and tmap[char] == smap[char]: # counts match
                 formed += 1
             
-            while formed == len(need):
-                if right - left + 1 < min_len:
-                    min_len = right - left + 1
-                    ans = s[left:right + 1]
-                
-                window[s[left]] -= 1
+            while left <= right and formed == len(tmap): # as long as its valid
+                char = s[left]
 
-                if s[left] in need and window[s[left]] < need[s[left]]:
-                    formed -= 1
+                if right - left + 1 < ans[0]:
+                    ans = (right - left + 1, left, right)
+
+                smap[char] -= 1
+
+                if char in tmap and smap[char] < tmap[char]: # if it became invalid
+                    formed -= 1 # reduces once then stops since while loop stops
                 
                 left += 1
         
-        return ans
+        return "" if ans[0] == float("inf") else s[ans[1]:ans[2]+1]
