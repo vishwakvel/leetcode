85b3bf16cdf2f0a5053d870ec3e4f0a7e43831fa
Vishwak Velamuri <45471435+vishwakvel@users.noreply.github.com>
2026-07-19 00:47:38 +0000

    [LeetCode Sync] Runtime - 8 ms (34.33%), Memory - 23 MB (41.82%)

--- leetcode/0347-top-k-frequent-elements/solution.py
+++ leetcode/0347-top-k-frequent-elements/solution.py
@@ -1,17 +1,17 @@
+import heapq
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        """
-        1. Convert nums to a counter
-        2. Use counter's built in method most_common to get the k most common numbers
-        3. Use a for loop to extract just the first element of each element in 2s output because most_common return not just the most common elements but also how many of each there are
-        """
+        hashmap = defaultdict(int)
+        heap = []
+
+        for num in nums:
+            hashmap[num] += 1
         
-        from collections import Counter
+        for num in hashmap:
+            heapq.heappush(heap, (hashmap[num], num))
 
-        ans = []
-        nums_counter = Counter(nums)
-
-        for l in nums_counter.most_common(k):
-            ans.append(l[0])
-
-        return ans
+            if len(heap) > k:
+                heapq.heappop(heap)
+        
+        return [element for freq, element in heap]
