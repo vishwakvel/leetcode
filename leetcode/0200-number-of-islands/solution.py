f433dee5b101dfe2bcde3d569a156a6f50073a67
Vishwak Velamuri <45471435+vishwakvel@users.noreply.github.com>
2026-07-19 15:09:54 +0000

    [LeetCode Sync] Runtime - 263 ms (24.18%), Memory - 21.7 MB (36.05%)

--- leetcode/0200-number-of-islands/solution.py
+++ leetcode/0200-number-of-islands/solution.py
@@ -2,23 +2,28 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         m = len(grid)
         n = len(grid[0])
-        dirs = [(0, -1), (-1, 0), (1, 0), (0, 1)]
+        dirs = [(0, 1), (1, 0), (-1, 0), (0, -1)]
 
-        def dfs(r, c):
-            if r >= m or c >= n or r < 0 or c < 0 or grid[r][c] == "0":
-                return False
+        def dfs(row, col):
+            if row < 0 or col < 0 or row >= m or col >= n:
+                return
             
-            grid[r][c] = "0"
-
+            if grid[row][col] == "0":
+                return
+            
+            grid[row][col] = "0" # was island now change to 0 to mark visited
+            
             for dr, dc in dirs:
-                dfs(r+dr, c+dc)
+                nr = dr+row
+                nc = dc+col
+                dfs(nr, nc)
         
         islands = 0
 
-        for row in range(m):
-            for col in range(n):
-                if grid[row][col] == "1":
-                    dfs(row, col)
+        for r in range(m):
+            for c in range(n):
+                if grid[r][c] == "1":
                     islands += 1
+                    dfs(r, c)
         
         return islands
