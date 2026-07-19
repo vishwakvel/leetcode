8a2bddcaac38853ae0d6a0c1820c2a32ea23cd60
Vishwak Velamuri <45471435+vishwakvel@users.noreply.github.com>
2026-07-19 15:31:59 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.6 MB (39.70%)

--- leetcode/0399-evaluate-division/solution.py
+++ leetcode/0399-evaluate-division/solution.py
@@ -2,14 +2,15 @@ class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
         graph = defaultdict(list)
 
-        for i, (start, end) in enumerate(equations):
+        for i in range(len(equations)):
+            start, end = equations[i]
             graph[start].append((end, values[i]))
             graph[end].append((start, 1/values[i]))
         
         def dfs(start, end, visited):
             if start == end:
                 return 1.0
-
+            
             visited.add(start)
             
             for neighbor, weight in graph[start]:
@@ -18,14 +19,14 @@ class Solution:
 
                     if result != -1:
                         return weight * result
-                
+            
             return -1
         
         ans = []
 
         for start, end in queries:
             if start not in graph or end not in graph:
-                ans.append(-1.0)
+                ans.append(-1)
             else:
                 ans.append(dfs(start, end, set()))
         
