3e9591d382c19f7851ee6a57b4147e624bed506d
Vishwak Velamuri <45471435+vishwakvel@users.noreply.github.com>
2026-07-19 02:42:10 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 21.2 MB (25.41%)

--- leetcode/0207-course-schedule/solution.py
+++ leetcode/0207-course-schedule/solution.py
@@ -1,26 +1,25 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        # detect cycles
         graph = defaultdict(list)
-        
+
         for course, prereq in prerequisites:
             graph[prereq].append(course)
         
         states = [0] * numCourses
-
+        
         def dfs(course):
             if states[course] == 1:
                 return False
             
             if states[course] == 2:
                 return True
-        
-            states[course] = 1
             
+            states[course] = 1
+
             for neighbor in graph[course]:
                 if not dfs(neighbor):
                     return False
-                
+            
             states[course] = 2
             return True
         
