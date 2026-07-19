abcbf271e9d7caa89e849a9b6a51285fc4a9f74d
Vishwak Velamuri <45471435+vishwakvel@users.noreply.github.com>
2026-07-19 15:41:15 +0000

    [LeetCode Sync] Runtime - 283 ms (52.05%), Memory - 20.6 MB (56.99%)

--- leetcode/0127-word-ladder/solution.py
+++ leetcode/0127-word-ladder/solution.py
@@ -3,7 +3,7 @@ from collections import deque
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
         visited = set(wordList)
-
+        
         if endWord not in visited:
             return 0
 
@@ -16,8 +16,8 @@ class Solution:
                 return steps
             
             for i in range(len(word)):
-                for c in "abcdefghijklmnopqrstuvwxyz":
-                    new = word[:i] + c + word[i+1:]
+                for char in "abcdefghijklmnopqrstuvwxyz":
+                    new = word[:i] + char + word[i+1:]
 
                     if new in visited:
                         visited.remove(new)
