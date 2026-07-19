4a6a22b1d81fb9b98af3f4d92e73a243cdec928d
Vishwak Velamuri <45471435+vishwakvel@users.noreply.github.com>
2026-07-19 21:14:17 +0000

    [LeetCode Sync] Runtime - 4 ms (76.72%), Memory - 19.8 MB (85.81%)

--- leetcode/0860-design-circular-queue/solution.py
+++ leetcode/0860-design-circular-queue/solution.py
@@ -2,28 +2,27 @@ class MyCircularQueue:
 
     def __init__(self, k: int):
         self.queue = [0] * k
-        self.size = 0
-        self.k = k
         self.front = 0
         self.rear = 0
-
+        self.k = k
+        self.size = 0
 
     def enQueue(self, value: int) -> bool:
         if self.isFull():
             return False
-        else:
-            self.queue[self.rear] = value
-            self.rear = (self.rear + 1) % self.k
-            self.size += 1
-            return True
+        
+        self.queue[self.rear] = value
+        self.rear = (self.rear + 1) % self.k
+        self.size += 1
+        return True
 
     def deQueue(self) -> bool:
         if self.isEmpty():
             return False
-        else:
-            self.front = (self.front + 1) % self.k
-            self.size -= 1
-            return True
+        
+        self.front = (self.front + 1) % self.k
+        self.size -= 1
+        return True
 
     def Front(self) -> int:
         if self.isEmpty():
@@ -36,7 +35,6 @@ class MyCircularQueue:
             return -1
         
         return self.queue[(self.rear - 1) % self.k]
-        
 
     def isEmpty(self) -> bool:
         return self.size == 0
