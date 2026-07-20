74c0474384d09048f6ee990062ed93023e3953f5
Vishwak Velamuri <45471435+vishwakvel@users.noreply.github.com>
2026-07-20 00:15:02 +0000

    [LeetCode Sync] Runtime - 72 ms (97.21%), Memory - 57.1 MB (22.35%)

--- leetcode/0380-insert-delete-getrandom-o1/solution.py
+++ leetcode/0380-insert-delete-getrandom-o1/solution.py
@@ -3,28 +3,30 @@ import random
 class RandomizedSet:
 
     def __init__(self):
-        self.rset = set()
+        self.array = []
+        self.hashmap = {}
 
     def insert(self, val: int) -> bool:
-        if val in self.rset:
-            return False 
-        else:
-            self.rset.add(val)
-            return True
-        
-
-    def remove(self, val: int) -> bool:
-        if val in self.rset:
-            self.rset.remove(val)
-            return True 
-        else:
+        if val in self.hashmap:
             return False
         
+        self.array.append(val)
+        self.hashmap[val] = len(self.array) - 1
+        return True
+
+    def remove(self, val: int) -> bool:
+        if val not in self.hashmap:
+            return False
+        
+        index = self.hashmap[val]
+        self.array[-1], self.array[index] = self.array[index], self.array[-1]
+        self.hashmap[self.array[index]] = index
+        self.array.pop()
+        del self.hashmap[val]
+        return True
 
     def getRandom(self) -> int:
-        return random.choice(list(self.rset))
-        
-
+        return random.choice(self.array)
 
 
 # Your RandomizedSet object will be instantiated and called as such:
