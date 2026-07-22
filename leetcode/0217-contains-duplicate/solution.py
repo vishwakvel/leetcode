2dbf5d27d2e92915b9295b7ea12b32b79a302e1a
Vishwak Velamuri <45471435+vishwakvel@users.noreply.github.com>
2026-07-22 19:02:46 +0000

    [LeetCode Sync] Runtime - 19 ms (40.80%), Memory - 32.3 MB (33.18%)

--- leetcode/0217-contains-duplicate/solution.py
+++ leetcode/0217-contains-duplicate/solution.py
@@ -1,10 +1,11 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        """
-        1. Convert nums list into set
-        2. Check if the set and nums list have the different length
-            a) If they do then true
-            b) If they don't then false
-        """
-        nums_set = set(nums)
-        return len(nums_set) != len(nums)
+        seen = set()
+
+        for num in nums:
+            if num in seen:
+                return True
+            else:
+                seen.add(num)
+        
+        return False
