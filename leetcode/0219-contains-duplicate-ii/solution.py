51a10fce6e5f02934b8808c5c6934f59e10ba451
Vishwak Velamuri <45471435+vishwakvel@users.noreply.github.com>
2026-07-22 21:36:43 +0000

    [LeetCode Sync] Runtime - 52 ms (20.54%), Memory - 32.2 MB (88.66%)

--- leetcode/0219-contains-duplicate-ii/solution.py
+++ leetcode/0219-contains-duplicate-ii/solution.py
@@ -1,20 +1,14 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        """
-        1. Create a defaultdict to keep track of numbers we've already been through. The key is the number, and value is the last index they were seen at.
-        2. Iterate through numbers and for each number check if it is in dict
-            a) If it is, then check if it satisfies the condition (current index and last seen index, which is value in the dict, difference <= k, then True)
-            b) If it isn't, continue
-        3. Set dict's current num (key)'s value to current index
-        4. If true hasn't been returned yet, no indices matched, so return False
-        """
-        seen = defaultdict(int)
+        window = set()
 
         for i in range(len(nums)):
-            if nums[i] in seen:
-                if i - seen[nums[i]] <= k:
-                    return True
-                
-            seen[nums[i]] = i
-        
+            if nums[i] in window:
+                return True
+
+            window.add(nums[i])
+
+            if len(window) > k:
+                window.remove(nums[i - k])
+
         return False
