a59c5542d315141521c0059fbb0755c5764afedd
Vishwak Velamuri <45471435+vishwakvel@users.noreply.github.com>
2026-07-22 17:15:06 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 23.2 MB (28.28%)

--- leetcode/0056-merge-intervals/solution.py
+++ leetcode/0056-merge-intervals/solution.py
@@ -1,15 +1,16 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        # my logic is comparing 2 intervals if start1 < end2 and start2 < end1 then they overlap. sort list first since we want to keep track of earliest first
         intervals.sort(key=lambda x: x[0])
-
         ans = [intervals[0]]
 
-        for start, end in intervals[1:]:
-            prev = ans[-1]
+        for i in range(1, len(intervals)):
+            prevstart, prevend = ans[-1]
+            currstart, currend = intervals[i]
 
-            if start <= prev[1]:
-                prev[1] = max(prev[1], end)
+            if currstart <= prevend and prevstart <= currend:
+                ans[-1] = [prevstart, max(prevend, currend)]
             else:
-                ans.append([start, end])
+                ans.append([currstart, currend])
         
         return ans
