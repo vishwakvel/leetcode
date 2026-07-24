b66f5f427d2ffb2346edcf4496d15c1da853c0f1
Vishwak Velamuri <45471435+vishwakvel@users.noreply.github.com>
2026-07-24 13:46:16 +0000

    [LeetCode Sync] Runtime - 706 ms (47.27%), Memory - 56.5 MB (29.70%)

---
+++ leetcode/3303-find-beautiful-indices-in-the-given-array-ii/solution.py
@@ -0,0 +1,50 @@
+class Solution:
+    def beautifulIndices(self, s: str, a: str, b: str, k: int) -> List[int]:
+        def kmp(text, pattern):
+            lps = [0] * len(pattern)
+            prev = 0
+            i = 1
+
+            while i < len(pattern):
+                if pattern[i] == pattern[prev]:
+                    prev += 1
+                    lps[i] = prev
+                    i += 1
+                elif prev > 0:
+                    prev = lps[prev - 1]
+                else:
+                    lps[i] = 0
+                    i += 1
+            
+            result = []
+            t = 0 # text pointer
+            p = 0 # pattern pointer
+
+            while t < len(text):
+                if pattern[p] == text[t]:
+                    t += 1
+                    p += 1
+
+                    if p == len(pattern):
+                        result.append(t - p)
+                        p = lps[p-1]
+                elif p > 0:
+                    p = lps[p - 1]
+                else:
+                    t += 1
+            
+            return result
+        
+        aindices = kmp(s, a)
+        bindices = kmp(s, b)
+        b = 0
+        ans = []
+
+        for a in aindices:
+            while b < len(bindices) and bindices[b] < a - k:
+                b += 1
+            
+            if b < len(bindices) and bindices[b] <= a + k:
+                ans.append(a)
+        
+        return ans
