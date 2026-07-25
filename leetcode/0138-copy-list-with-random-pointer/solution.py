0c2bdd6ebb51100e630e88c6a3c3d96bd35b8dd4
Vishwak Velamuri <45471435+vishwakvel@users.noreply.github.com>
2026-07-25 00:16:44 +0000

    [LeetCode Sync] Runtime - 54 ms (17.99%), Memory - 20 MB (57.85%)

--- leetcode/0138-copy-list-with-random-pointer/solution.py
+++ leetcode/0138-copy-list-with-random-pointer/solution.py
@@ -12,19 +12,18 @@ class Solution:
         if not head:
             return None
 
-        hmap = {} # old to new node
-        curr = head
-        
-        while curr:
-            node = Node(curr.val, curr.next, curr.random)
-            hmap[curr] = node
-            curr = curr.next
-        
+        hashmap = {} # old to new node
         curr = head
 
         while curr:
-            hmap[curr].next = hmap.get(curr.next)
-            hmap[curr].random = hmap.get(curr.random)
+            hashmap[curr] = Node(curr.val, curr.next, curr.random)
             curr = curr.next
         
-        return hmap[head]
+        curr = head
+        
+        while curr:
+            hashmap[curr].next = hashmap.get(curr.next)
+            hashmap[curr].random = hashmap.get(curr.random)
+            curr = curr.next
+        
+        return hashmap[head]
