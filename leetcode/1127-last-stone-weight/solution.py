98be77c070e63f41d52a3f0a724f21d92330a02c
Vishwak Velamuri <45471435+vishwakvel@users.noreply.github.com>
2026-07-25 18:17:48 +0000

    [LeetCode Sync] Runtime - 3 ms (9.68%), Memory - 19.5 MB (35.70%)

--- leetcode/1127-last-stone-weight/solution.py
+++ leetcode/1127-last-stone-weight/solution.py
@@ -1,13 +1,15 @@
+import heapq
+
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
-        stones = [-s for s in stones]
+        stones = [-stone for stone in stones]
         heapq.heapify(stones)
 
         while len(stones) > 1:
-            y = -heapq.heappop(stones)
-            x = -heapq.heappop(stones)
+            x = heapq.heappop(stones)
+            y = heapq.heappop(stones)
 
             if x != y:
-                heapq.heappush(stones, -(y-x))
+                heapq.heappush(stones, x-y)
         
         return -stones[0] if stones else 0
