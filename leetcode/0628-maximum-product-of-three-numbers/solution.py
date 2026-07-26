9a9c0f007fb82ed570ce57154c71ffdda68409c6
Vishwak Velamuri <45471435+vishwakvel@users.noreply.github.com>
2026-07-26 17:56:03 +0000

    [LeetCode Sync] Runtime - 38 ms (5.37%), Memory - 20.3 MB (44.24%)

--- leetcode/0628-maximum-product-of-three-numbers/solution.py
+++ leetcode/0628-maximum-product-of-three-numbers/solution.py
@@ -1,4 +1,16 @@
 class Solution:
     def maximumProduct(self, nums: List[int]) -> int:
-        nums.sort(reverse = True)
-        return max(nums[0] * nums[1] * nums[2], nums[0] * nums[-1] * nums[-2])
+        first = second = third = float("-inf")
+        last = slast = float("inf")
+
+        for n in nums:
+            pfirst, psecond, plast = first, second, last
+
+            first = max(first, n)
+            second = max(second, min(pfirst, n))
+            third = max(third, min(psecond, n))
+
+            last = min(last, n)
+            slast = min(slast, max(plast, n))
+
+        return max(first * second * third, first * last * slast)
