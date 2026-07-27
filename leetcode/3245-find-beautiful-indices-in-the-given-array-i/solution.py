75493c870dd8a2d3665afc4d6eb4fb7eead2d5fb
Vishwak Velamuri <45471435+vishwakvel@users.noreply.github.com>
2026-07-27 14:26:08 +0000

    [LeetCode Sync] Runtime - 111 ms (69.98%), Memory - 23.3 MB (73.52%)

--- leetcode/3245-find-beautiful-indices-in-the-given-array-i/solution.py
+++ leetcode/3245-find-beautiful-indices-in-the-given-array-i/solution.py
@@ -4,20 +4,20 @@ class Solution:
         bindices = []
 
         for i in range(len(s)):
-            if s[i: i+len(a)] == a:
+            if s[i:i+len(a)] == a:
                 aindices.append(i)
             
-            if s[i: i+len(b)] == b:
+            if s[i:i+len(b)] == b:
                 bindices.append(i)
         
-        ans = []
         j = 0
+        ans = []
 
         for i in aindices:
-            while j < len(bindices) and bindices[j] < i - k:
+            while j < len(bindices) and bindices[j] < i-k:
                 j += 1
             
-            if j < len(bindices) and bindices[j] <= i + k:
+            if j < len(bindices) and bindices[j] <= i+k:
                 ans.append(i)
         
         return ans
