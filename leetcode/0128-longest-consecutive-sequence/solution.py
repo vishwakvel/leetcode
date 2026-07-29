f2fa2d30384d4c4d0a02632fdb36b49259100727
Vishwak Velamuri <45471435+vishwakvel@users.noreply.github.com>
2026-07-29 23:54:33 +0000

    [LeetCode Sync] Runtime - 43 ms (85.16%), Memory - 36.5 MB (66.43%)

--- leetcode/0128-longest-consecutive-sequence/solution.py
+++ leetcode/0128-longest-consecutive-sequence/solution.py
@@ -1,23 +1,15 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        """
-        1. Convert to set to remove duplicates
-        2. Go through each number in the set
-        3. Check if theres a number right before it thats already in the set
-            a) If there is, then skip the current number because it will already be checked by the number previous
-            b) If there isn't, then set length = 1 and then increment length by 1 for each num+length (length changes by 1 every time so it looks at consecutive elements). Longest is also set to the max of the current length and the previous longest, thus making sure the longest sequence is stored and not the most current
-        4. Return longest
-        """
-        nums_set = set(nums)
-        longest = 0
+        numsset = set(nums)
+        ans = 0
 
-        for num in nums_set:
-            if num - 1 not in nums_set:
+        for num in numsset:
+            if num-1 not in numsset:
                 length = 1
 
-                while num + length in nums_set:
+                while num + length in numsset:
                     length += 1
                 
-                longest = max(longest, length)
+                ans = max(ans, length)
         
-        return longest
+        return ans
