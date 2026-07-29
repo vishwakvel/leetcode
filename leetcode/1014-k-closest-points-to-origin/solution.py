00a02256c2664ccfeaee756d05e25a996a6cab53
Vishwak Velamuri <45471435+vishwakvel@users.noreply.github.com>
2026-07-29 18:36:33 +0000

    [LeetCode Sync] Runtime - 5126 ms (5.03%), Memory - 23.8 MB (95.24%)

--- leetcode/1014-k-closest-points-to-origin/solution.py
+++ leetcode/1014-k-closest-points-to-origin/solution.py
@@ -1,12 +1,50 @@
+import heapq
+
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        ans = []
-        distances = [(x**2 + y**2, [x,y]) for x,y in points]
-        heapq.heapify(distances)
+        """
+        heap = []
 
-        while k:
-            closest = heapq.heappop(distances)[1]
-            ans.append(closest)
-            k -= 1
+        for x, y in points:
+            dist = x*x + y*y
+
+            heapq.heappush(heap, (-dist, [x,y]))
+
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        return [point for _, point in heap]
+        """
+        import random
+
+        def dist(point):
+            return point[0] * point[0] + point[1] * point[1]
+
+        def partition(left, right):
+            index = random.randint(left, right)
+            points[index], points[right] = points[right], points[index]
+            pivotdist = dist(points[right])
+            store = left
+
+            for i in range(left, right):
+                if dist(points[i]) < pivotdist:
+                    points[store], points[i] = points[i], points[store]
+                    store += 1
+            
+            points[store], points[right] = points[right], points[store]
+            return store
         
-        return ans
+        left = 0
+        right = len(points) - 1
+
+        while left <= right:
+            index = partition(left, right)
+
+            if index == k:
+                break
+            elif index < k:
+                left = index + 1
+            else:
+                right = index - 1
+        
+        return points[:k]
