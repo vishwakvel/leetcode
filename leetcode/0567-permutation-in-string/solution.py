e647ee782540c5b9e2a486f25831b2c2ef016c66
Vishwak Velamuri <45471435+vishwakvel@users.noreply.github.com>
2026-07-29 19:32:48 +0000

    [LeetCode Sync] Runtime - 65 ms (22.69%), Memory - 19.2 MB (86.46%)

--- leetcode/0567-permutation-in-string/solution.py
+++ leetcode/0567-permutation-in-string/solution.py
@@ -1,13 +1,26 @@
+from collections import Counter
+
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        size = len(s1)
-        counter_s1 = Counter(s1)
+        if len(s1) > len(s2):
+            return False
+
+        target = Counter(s1)
+        window = Counter()
         left = 0
 
-        for right in range(len(s2) - size + 1):
-            window_counter = Counter(s2[right:right+size])
-            
-            if counter_s1 == window_counter:
+        for right in range(len(s2)):
+            window[s2[right]] += 1
+
+            if right - left + 1 > len(s1):
+                window[s2[left]] -= 1
+                
+                if window[s2[left]] == 0:
+                    del window[s2[left]]
+
+                left += 1
+
+            if window == target:
                 return True
-        
+
         return False
