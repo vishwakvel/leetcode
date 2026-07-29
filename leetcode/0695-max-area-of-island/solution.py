22d25fc375ebff9e062a881aac1ad5084ea47a06
Vishwak Velamuri <45471435+vishwakvel@users.noreply.github.com>
2026-07-29 01:18:19 +0000

    [LeetCode Sync] Runtime - 19 ms (61.60%), Memory - 20.5 MB (53.24%)

--- leetcode/0695-max-area-of-island/solution.py
+++ leetcode/0695-max-area-of-island/solution.py
@@ -1,9 +1,10 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        ans = 0
         m = len(grid)
         n = len(grid[0])
         dirs = [(0, 1), (1, 0), (-1, 0), (0, -1)]
-        
+
         def dfs(row, col):
             if row < 0 or col < 0 or row >= m or col >= n or grid[row][col] == 0:
                 return 0
@@ -12,17 +13,13 @@ class Solution:
             area = 1
 
             for dr, dc in dirs:
-                nr = row + dr
-                nc = col + dc
-
-                area += dfs(nr, nc)
+                area += dfs(row+dr, col+dc)
             
             return area
         
-        maxarea = 0
-        
         for r in range(m):
             for c in range(n):
-                maxarea = max(maxarea, dfs(r, c))
+                if grid[r][c] == 1:
+                    ans = max(ans, dfs(r, c))
         
-        return maxarea
+        return ans
