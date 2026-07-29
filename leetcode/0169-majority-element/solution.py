8a6950e9d5a845131814398d66c4af150b84b2fe
Vishwak Velamuri <45471435+vishwakvel@users.noreply.github.com>
2026-07-29 13:26:40 +0000

    [LeetCode Sync] Runtime - 4 ms (70.98%), Memory - 21.2 MB (46.98%)

--- leetcode/0169-majority-element/solution.py
+++ leetcode/0169-majority-element/solution.py
@@ -6,17 +6,10 @@ class Solution:
         for num in nums:
             if count == 0:
                 candidate = num
-                count += 1
-            elif num == candidate:
+                
+            if num == candidate:
                 count += 1
             else:
                 count -= 1
         
-        count = 0
-
-        for num in nums:
-            if num == candidate:
-                count += 1
-        
-        if count > len(nums)//2:
-            return candidate
+        return candidate
