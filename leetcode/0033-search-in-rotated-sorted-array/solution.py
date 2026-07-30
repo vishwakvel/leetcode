0c441b8e67ce2a118cd5e5b9f2fe7515655cafae
Vishwak Velamuri <45471435+vishwakvel@users.noreply.github.com>
2026-07-30 14:06:53 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.4 MB (77.74%)

--- leetcode/0033-search-in-rotated-sorted-array/solution.py
+++ leetcode/0033-search-in-rotated-sorted-array/solution.py
@@ -6,16 +6,16 @@ class Solution:
         while left <= right:
             mid = (left + right) // 2
 
-            if target == nums[mid]:
+            if nums[mid] == target:
                 return mid
 
             if nums[left] <= nums[mid]:
-                if nums[left] <= target and target < nums[mid]:
+                if nums[left] <= target < nums[mid]:
                     right = mid - 1
                 else:
                     left = mid + 1
             else:
-                if nums[mid] < target and target <= nums[right]:
+                if nums[mid] < target <= nums[right]:
                     left = mid + 1
                 else:
                     right = mid - 1
